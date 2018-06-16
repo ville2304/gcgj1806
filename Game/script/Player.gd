e830507a5a14cd9c0675ac060e4598010ad622d8
Ville Laine <ville2304@gmail.com>
2018-06-16 19:29:51 +0300

    Added temperature HP, cooldown and death event.

--- Game/script/Player.gd
+++ Game/script/Player.gd
@@ -23,28 +23,112 @@ extends KinematicBody
 const DEAD_ZONE = 0.2 * 0.2
 const ZERO_ANGLE = Vector2(-1, 0)
 
+const NATURAL_TEMP_RISE = 4.0
+const NATURAL_TEMP_DECREASE = 0.5
+const COOLDOWN_TEMP_DECREASE_MAX = 10.0
+
+enum Mode{
+	IDLE
+	WALK
+	COOLDOWN
+}
+
+
 onready var mObject = $Spatial
 
+var Dead setget ,isDead
+func isDead():
+	return Dead
+
+var mMode
+var mTemperature
+var mCooldownTimer
+var mSupercharged
+onready var mLabel = get_node("../Label")
+
+
+func Init():
+	mMode = Mode.IDLE
+	Dead = false
+	mTemperature = 20.0
+	mSupercharged = false
+	mLabel.text = str(mTemperature)
+
+func _ready():
+	Init()
 
 func _physics_process(delta):
-	var mvec = Vector3()
-	if Input.is_action_pressed("move_north"):
-		mvec.z -= 1
-	if Input.is_action_pressed("move_south"):
-		mvec.z += 1
-	if Input.is_action_pressed("move_east"):
-		mvec.x += 1
-	if Input.is_action_pressed("move_west"):
-		mvec.x -= 1
+	if Dead:
+		return
 	
-	var axis = Vector2(Input.get_joy_axis(0, JOY_ANALOG_LX), Input.get_joy_axis(0, JOY_ANALOG_LY))
-	if axis.length_squared() >= DEAD_ZONE:
-		mvec.x = axis.x
-		mvec.z = axis.y
+	if Input.is_action_just_pressed("cooldown"):
+		# TODO: Play cooldown animation
+		print("anim cooldown")
+		mMode = Mode.COOLDOWN
+		mCooldownTimer = 0
+	if Input.is_action_just_released("cooldown"):
+		# TODO: Play cooldown off animation
+		print("anim cooldown off")
+		mMode = Mode.IDLE
+	
+	if mMode == Mode.IDLE || mMode == Mode.WALK:
+		var mvec = Vector3()
+		if Input.is_action_pressed("move_north"):
+			mvec.z -= 1
+		if Input.is_action_pressed("move_south"):
+			mvec.z += 1
+		if Input.is_action_pressed("move_east"):
+			mvec.x += 1
+		if Input.is_action_pressed("move_west"):
+			mvec.x -= 1
 		
-	mvec = mvec.normalized() * 4.0
-	move_and_slide(mvec)
-	if mvec.length_squared() != 0:
-		mObject.rotation.y = Vector2(mvec.x, mvec.z).angle_to(ZERO_ANGLE)
+		var axis = Vector2(Input.get_joy_axis(0, JOY_ANALOG_LX), Input.get_joy_axis(0, JOY_ANALOG_LY))
+		if axis.length_squared() >= DEAD_ZONE:
+			mvec.x = axis.x
+			mvec.z = axis.y
+		mvec = mvec.normalized() * 4.0
+		if mvec.length_squared() != 0:
+			if mMode != Mode.WALK:
+				# TODO: Play anim walk
+				print("anim walk")
+				mMode = Mode.WALK
+			move_and_slide(mvec)
+			mObject.rotation.y = Vector2(mvec.x, mvec.z).angle_to(ZERO_ANGLE)
+		else:
+			if mMode != Mode.IDLE:
+				# TODO: Play anim idle
+				print("anim idle")
+				mMode = Mode.IDLE
+	
+	# Temperature control
+	if mMode == Mode.WALK:
+		mTemperature += NATURAL_TEMP_RISE * delta
+	elif mMode == Mode.IDLE:
+		if !mSupercharged:
+			mTemperature -= NATURAL_TEMP_DECREASE * delta
+	elif mMode == Mode.COOLDOWN:
+		mCooldownTimer += delta * .2
+		# TODO: Add nifty effect here
+		mTemperature -= min(mCooldownTimer, COOLDOWN_TEMP_DECREASE_MAX)
+	
+	if mTemperature <= 0:
+		# TODO: Play anim death
+		print("anim death")
+		Dead = true
+		# TODO: Broadcast death
+	mTemperature = clamp(mTemperature, 0, 100)
+	
+	if mTemperature >= 100:
+		if !mSupercharged:
+			# TODO: Play anim supercharge
+			print("anim supercharge")
+		mSupercharged = true
+	else:
+		if mSupercharged:
+			# TODO: Play anim supercharge off
+			print("anim supercharge off")
+		mSupercharged = false
+	
+	mLabel.text = str(mTemperature)
 
 
